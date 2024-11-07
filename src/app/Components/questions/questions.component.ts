ef8d858cf4d5c3795149a724cbbdb1bc1f894899
InakiEguia113128 <90732757+InakiEguia113128@users.noreply.github.com>
2024-11-07 10:32:59 -0300

    Términos y condiciones

--- src/app/Components/questions/questions.component.ts
+++ src/app/Components/questions/questions.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { UserService } from 'src/app/Services/Users/user.service';
 
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
   styleUrls: ['./questions.component.css']
 })
-export class QuestionsComponent {
+export class QuestionsComponent implements OnInit {
+
+  esSocio : boolean = false;
+
+  constructor(
+    private readonly servicioUsuario: UserService,
+    private readonly spinner: NgxSpinnerService,
+  ) { }
+  
+  ngOnInit(): void {
+    debugger
+    const rolesUsuario = this.servicioUsuario.obtenerRolesUsuarioActivo();
+    const rolesValidos = ['Socio', 'Socio registrado'];
+    this.esSocio = rolesUsuario.roles.some((rol: any) => rolesValidos.includes(rol));
+  }
 }
